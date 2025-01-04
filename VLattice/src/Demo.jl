fff86dfaa19bea547be67fe2a807e77cf6b88440
tatjam <tatjam02@gmail.com>
2025-01-05 00:57:20 +0100

    Progress

--- VLattice/src/Demo.jl
+++ VLattice/src/Demo.jl
@@ -6,7 +6,31 @@ function xcenter(quad)
     return 0.25 * sum(map(vert -> coords(vert).x, vertices(quad)))
 end
 
-# Ad-hoc and inefficient
+# Only camber, not thickness included!
+function nacacamberlaw(nacanums, pos)
+    # Simply derivative of thickness function
+    t = (nacanums[3] * 10 + nacanums[4]) / 100
+    thickprime = 5t * (-0.126 + 0.14845 / sqrt(pos) - 0.7032pos + 0.8529pos^2 - 0.406pos^3)
+
+    # note, derivative of the position is dy/dx, thus angle of slope is 
+    # the arctangent of this value!
+    if nacanums[1] == 0 && nacanums[2] == 0
+        return 0.0
+    else
+        m = nacanums[1] / 100
+        p = nacanums[2] / 10
+        camberprime = 0.0
+        if pos <= p
+            camberprime = m / p^2 * (2p - 2pos)
+        else
+            camberprime = m / (1 - p)^2 * (2p - 2pos)
+        end
+
+        return atan(camberprime)
+    end
+end
+
+# Ad-hoc and inefficient. 
 function spanwise_sln(mesh, sln)
     X = []
     Y = []
@@ -30,11 +54,17 @@ function spanwise_sln(mesh, sln)
     return ustrip(hcat(X, Y))
 end
 
+function visualizescalar(comb, slopes)
+    display(viz(comb, color=slopes))
+end
+
 function demo(α)
-    grid1 = wingmesh(50, 50, 15, 1.9, 0.8, deg2rad(15))
-    grid2 = wingmesh(40, 40, 7.5, 0.9, 0.6, deg2rad(10))
+    grid1, slopes1 = wingmesh(40, 40, 15, 1.9, 0.8, deg2rad(15), x -> nacacamberlaw([4, 4, 1, 6], x))
+    grid2, slopes2 = wingmesh(30, 30, 7.5, 0.9, 0.6, deg2rad(10), x -> nacacamberlaw([0, 0, 1, 8], x))
     grid2 = Translate(0, 7)(grid2)
     comb = grid1 ∪ grid2
+
+    slopes = vcat(slopes1, slopes2)
 
     θs = zeros(length(comb))
 
@@ -42,10 +72,14 @@ function demo(α)
     for (i, elem) in enumerate(grid1)
         x = xcenter(elem)
         xadim = x / 15
-        θs[i] = abs(ustrip(xadim)) * deg2rad(5)
+        θs[i] = 2.0 * abs(ustrip(xadim)) * deg2rad(5)
     end
 
-    sln = solve(comb, α, θs)
-    return comb, sln
+    rh = rhs(mesh, α, slopes, θs)
+    #visualizescalar(comb, rh)
+
+    sln = solve(comb, α, slopes, θs)
+
     #display(viz(comb, color=log.(abs.(sln))))
+    return coefficients(comb, sln, α, 0)
 end
--- VLattice/src/VLattice.jl
+++ VLattice/src/VLattice.jl
@@ -3,29 +3,36 @@ module VLattice
 using Meshes
 using Unitful
 using GLMakie
+using LinearAlgebra
 
 export wingmesh
 export quad2elem
 export influence
 export influencemat
 export solve
+export coefficients
 
 include("Demo.jl")
 
 """
-	wingmesh(nspanwise, nchordwise, b, Γ, claw, cofflaw)
+	wingmesh(nspanwise, nchordwise, b, Γ, claw, cofflaw, camberlaw)
 
 Generates a structured mesh of given vertex numbers for a wing of span `b`, and with the given 
 functions `claw` and `cofflaw` which define the distribution of chord and local 1/4 point offset at 
 each spanwise position.
-These are functions of dimensional span position, going from -b/2 to b/2.
+These are functions of dimensional span position, going from -b/2 to b/2. The camberlaw is 
+instead given NON-DIMENSIONAL chord position, from 0 to 1
 
 The wing span-wise direction is assumed to be parallel to the x-axis.
 If cofflaw = 0, the 1/4 point of the chord will lie on the X axis.
 """
-function wingmesh(nspanwise::Integer, nchordwise::Integer, b::Number, claw::Function, cofflaw::Function)
+function wingmesh(nspanwise::Integer, nchordwise::Integer, b::Number, claw::Function,
+    cofflaw::Function, camberlaw::Function)
+
     X = zeros(nchordwise, nspanwise)
     Y = zeros(nchordwise, nspanwise)
+    slopes = zeros((nchordwise - 1) * (nspanwise - 1))
+
     for i in 1:nspanwise
         linprog = (i - 1) / (nspanwise - 1) # from 0 to 1
         # Adjust progression to accumulate vertices near the edges
@@ -46,10 +53,16 @@ function wingmesh(nspanwise::Integer, nchordwise::Integer, b::Number, claw::Func
             qpos = c * 0.25
             chordpos = c * (yprog - 0.5) + qpos + coff
             Y[yi, i] = chordpos
+
+            if i > 1 && i < nspanwise
+                if yi > 1 && yi < nchordwise
+                    slopes[(i-1)*(nchordwise-1)+(yi-1)] = camberlaw(yprog)
+                end
+            end
         end
     end
 
-    return StructuredGrid(X, Y)
+    return StructuredGrid(X, Y), slopes
 end
 
 """
@@ -58,28 +71,20 @@ end
 Same as generic `wingmesh` but generates the wing given its sweep in radians. Sweep is the angle 
 formed by the 1/4 chord points of the root and the tip of the wing.
 """
-function wingmesh(nspanwise::Integer, nchordwise::Integer, b::Number, claw::Function, Γ::Number)
-    return wingmesh(nspanwise, nchordwise, b, claw, x -> abs(x) * tan(Γ))
+function wingmesh(nspanwise::Integer, nchordwise::Integer, b::Number, claw::Function, Γ::Number, camberlaw::Function)
+    return wingmesh(nspanwise, nchordwise, b, claw, x -> abs(x) * tan(Γ), camberlaw)
 end
 
 """
-	wingmesh(nspawnwise, nchordwise, rootchord, tipchord, Γ)
+	wingmesh(nspawnwise, nchordwise, rootchord, tipchord, Γ, camberlaw)
 
 Same as generic `wingmesh` but generates the wing given its sweep in radians. Sweep is the angle 
 formed by the 1/4 chord points of the root and the tip of the wing.
 """
 function wingmesh(nspanwise::Integer, nchordwise::Integer, b::Number, rootchord::Number,
-    tipchord::Number, Γ::Number)
+    tipchord::Number, Γ::Number, camberlaw::Function)
     return wingmesh(nspanwise, nchordwise, b,
-        x -> rootchord - (rootchord - tipchord) * abs(x) / (0.5 * b), Γ)
-end
-
-function leading_trailing(pa, pb)
-    if coords(pa).y > coords(pb).y
-        return [pa, pb]
-    else
-        return [pb, pa]
-    end
+        x -> rootchord - (rootchord - tipchord) * abs(x) / (0.5 * b), Γ, camberlaw)
 end
 
 function testpoint(quad::Quadrangle)
@@ -90,7 +95,44 @@ function testpoint(quad::Quadrangle)
     ylead = 0.5 * (coords(elem[1]).y + coords(elem[4]).y)
     ytrail = 0.5 * (coords(elem[2]).y + coords(elem[3]).y)
 
-    return [ustrip(xmiddle), ustrip(ylead * 0.75 + ytrail * 0.25)]
+    return [ustrip(xmiddle), ustrip(ylead * 0.25 + ytrail * 0.75)]
+end
+
+function vortexpoints(quad::Quadrangle)
+    elem = vertices(quad)
+
+    return [
+        [ustrip(coords(elem[1]).x), ustrip(coords(elem[1]).y * 0.75 + coords(elem[2]).y * 0.25)],
+        [ustrip(coords(elem[4]).x), ustrip(coords(elem[4]).y * 0.75 + coords(elem[3]).y * 0.25)]
+    ]
+end
+
+function vortexpoint(quad::Quadrangle)
+    vpts = vortexpoints(quad)
+
+    return [
+        0.5 * (vpts[1][1] + vpts[2][1]),
+        0.5 * (vpts[1][2] + vpts[2][2])
+    ]
+end
+
+function levec(quad::Quadrangle)
+    elem = vertices(quad)
+
+    Δx = coords(elem[4]).x - coords(elem[1]).x
+    Δy = coords(elem[4]).y - coords(elem[1]).y
+
+    return [ustrip(Δx), ustrip(Δy), 0]
+end
+
+function lelen(quad::Quadrangle)
+    elem = vertices(quad)
+
+    Δx = coords(elem[4]).x - coords(elem[1]).x
+    Δy = coords(elem[4]).y - coords(elem[1]).y
+    l = sqrt(Δx * Δx + Δy * Δy)
+
+    return ustrip(l)
 end
 
 """
@@ -105,25 +147,29 @@ Assumes vertice are ordered as follows:
 - Leading edge, trailing edge, trailing edge, leading edge
 """
 function influence(cause::Quadrangle, tp)
-    celem = vertices(cause)
+    celem = vortexpoints(cause)
 
     # From class notes, but changing x for y
-    a = tp[2] - ustrip(coords(celem[1]).y)
-    b = tp[1] - ustrip(coords(celem[1]).x)
-    c = tp[2] - ustrip(coords(celem[4]).y)
-    d = tp[1] - ustrip(coords(celem[4]).x)
+    a = tp[2] - celem[1][2]
+    b = tp[1] - celem[1][1]
+    c = tp[2] - celem[2][2]
+    d = tp[1] - celem[2][1]
 
     e = sqrt(a^2 + b^2)
     f = sqrt(c^2 + d^2)
 
-    g = ustrip(coords(celem[4]).y) - ustrip(coords(celem[1]).y)
-    h = ustrip(coords(celem[4]).x) - ustrip(coords(celem[1]).x)
+    g = celem[2][2] - celem[1][2]
+    h = celem[2][1] - celem[1][1]
 
     val1 = (g * a + h * b) / e - (g * c + h * d) / f
     val2 = (c + f) / (d * f) - (a + e) / (b * e)
     val = val1 / (a * d - c * b) + val2
 
-    return val / (4 * π)
+    if isnan(val) || isinf(val)
+        return 0.0
+    end
+
+    return val / (4.0 * π)
 end
 
 """
@@ -156,29 +202,61 @@ end
 
 """
 
-We impose the condition that the flow is parallel to the tilted plane, which 
-roughly means that, taking `Vlocal = V∞ + Vinduced`, `tan(Vlocal_z / Vlocal_y)`
-is roughly equivalent to `Vinduced / V∞`, and this value must be equal to alpha.
-We set V∞ to 1, so the equation turns out to be Vinduced = alpha, so the right 
-hand side is simply alpha
+We impose the condition that the flow is parallel to the tilted plane, i.e. 
+vlocal⋅norm = 0 
 
-If θs is given, it must a vector as big as elements there are in the mesh, and each 
-θ represents an additional angle of attack induced upon the wing by geometric means
+where   vlocal = (0, cos(α), vind-sin(α))
+        norm = (0, sin(θ + slope), cos(θ + slope))
+
+thus vlocal⋅norm = sin(θ + slope)cos(α) + cos(θ + slope)*(vind - sin(α)) = 0
+we can reorder terms:
+    sin(α) - tan(θ + slope)cos(α) = vind
+where vind is each of our matrix rows
 
 """
-function rhs(mesh, α, θs=nothing)
+function rhs(mesh, α, slopes, θs=nothing)
     if isnothing(θs)
-        totalsize = length(mesh)
-        return fill(α, totalsize)
-    else
-        return θs .+ α
+        θs = zeros(length(mesh))
     end
+    return (sin(α) .- tan.(θs .- slopes) .* cos(α))
 end
 
-function solve(mesh, α, θs=nothing)
+function solve(mesh, α, slopes, θs=nothing)
     mat = influencemat(mesh)
-    rh = rhs(mesh, α, θs)
+    rh = rhs(mesh, α, slopes, θs)
     return mat \ rh
+end
+
+# returns cL, cDi, pitch moment (with respect to given point)
+# Assume 1m/s freestream velocity
+function coefficients(mesh, sln, α, moment0)
+    tot_surface = 0.0
+    force = [0.0, 0.0, 0.0]
+    forces = zeros(length(mesh))
+    for (i, elem) in enumerate(mesh)
+        s = ustrip(area(elem))
+        tot_surface += s
+
+        indvel = 0
+        for (otheri, otherelem) in enumerate(mesh)
+            if otheri != i
+                indvel += influence(otherelem, vortexpoint(elem)) * sln[otheri]
+            end
+        end
+
+        Vinf = [0, cos(α), indvel + sin(α)]
+        # This is really force / rho 
+        force .+= sln[i] .* cross(Vinf, levec(elem))
+        forces[i] = sln[i] * cross(Vinf, levec(elem))[3]
+    end
+
+    # project force
+    lift = force[2] * sin(α) + force[3] * cos(α)
+    drag = force[2] * cos(α) - force[3] * sin(α)
+
+    # cL / cD is force / 0.5 * rho * U^2 * S, thus we have to divide by surface and multiply by 2
+    return 2.0 * lift / tot_surface, 2.0 * drag / tot_surface
+
 end
 
 end # module VLattice
